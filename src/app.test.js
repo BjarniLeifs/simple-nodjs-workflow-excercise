e559d9e6cc8c83d7d55dd11ff7c149f97c057ff0
Bjarni Kristján Leifsson <bjarnikr@msn.com>
2023-09-19 08:44:03 +0000

    Update app.test.js

--- src/app.test.js
+++ src/app.test.js
@@ -4,7 +4,7 @@ const request = supertest(app)
 
 describe("/test endpoint", () => {
     it("should return a response", async () => {
-        const repsonse = await request.get("/test")
+        const response = await request.get("/test")
         expect(response.status).tobe(200)
         expect(response.text).tobe("Hello world");
     })
